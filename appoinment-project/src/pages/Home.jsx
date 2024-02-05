7a0a123cc860e6ef35ca0b9b351b84a13f21024d
SuhedaNurOzdemir <suhedanurozd99@gmail.com>
2024-02-05 17:17:36 +0300

    last commit

--- appoinment-project/src/pages/Home.jsx
+++ appoinment-project/src/pages/Home.jsx
@@ -8,19 +8,6 @@ const Home = () => {
   const [doctors, setDoctors] = useState(doctorData);
   const [appointments, setAppointments] = useState(appointmentData);
 
-  //! Mock Data yerine API'Den veri cekilseydi
-  // const getAppointments = async () => {
-  //   try {
-  //     const { data } = await axios.get("url")
-  //     setAppointments(data)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   getAppointments()
-  // }, [])
 
   console.log(appointments);
   console.log(doctors);
